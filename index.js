469044b1858d357f59074e777dce22eb8d2cc50d
The-Dark-Swordsman <kylebarr54321@gmail.com>
2017-11-11 14:31:55 -0500

    modified changelog to reflect current version

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const appPort = process.env.PORT || config.port;
 
 app.use('/', express.static('./src'));
 
+// authentication stuff can go below this
+
 app.listen(appPort, function(){
     console.log('safetravels running on port: ' + appPort);
 });
