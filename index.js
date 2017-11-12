4cba38e340e90903c6b6f103a418c43e2b363c73
The-Dark-Swordsman <kylebarr54321@gmail.com>
2017-11-12 03:30:15 -0500

    setup location

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const request = require('request'); 
 const bodyParser = require('body-parser');
+const async = require('async');
 
 const config = require('./config')(process.env.NODE_ENV);
 const appPort = process.env.PORT || config.port;
@@ -11,7 +12,17 @@ const appPort = process.env.PORT || config.port;
  * App Use
  */
 
-app.use('/', express.static('./src'));
+let source; // files to read
+let backendMode = false; // enable backend files
+
+if(backendMode === true){
+    source = './backend-test';
+    console.warn("BACKEND DEVELOPMENT IS ON")
+}else{
+    source = './src';
+}
+
+app.use('/', express.static(source));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
@@ -32,21 +43,61 @@ function getEvents(eventName, response){
 
         let eventsTemp = JSON.parse(res.body).events;
         let events = [];
+        let x = 0;
 
-        for(var i = 0; i < eventsTemp.length; i++){
-            events[i] = {
-                id: i,
-                name: eventsTemp[i].name.text,
-                desc: eventsTemp[i].description.text,
-                eventid: eventsTemp[i].id,
-                url: eventsTemp[i].url
-            }
+        for(var j = 0; j < eventsTemp.length; j++){
+            let i = j;
+            let eT = eventsTemp[i];
+            let lat;
+            let long;
+            let city;
+            let region;
+            let country;
+            request('https://www.eventbriteapi.com/v3/venues/' + eT.venue_id + '/?token=TOT2UEOZ2ZSLW5LM37FN', function(err, res, body){
+                let locationResponse = JSON.parse(body);
+
+                if(locationResponse.address != null){
+                    lat = locationResponse.latitude;
+                    long = locationResponse.longitude;
+                    city = locationResponse.address.city;
+                    region = locationResponse.address.region;
+                    country = locationResponse.address.country;
+
+            
+                    if(err){
+                        console.error(err.message);
+                    }
+                    
+                    events[i] = {
+                        id: i,
+                        name: eventsTemp[i].name.text,
+                        desc: eventsTemp[i].description.text,
+                        eventid: eventsTemp[i].id,
+                        url: eventsTemp[i].url,
+                        lat: lat,
+                        long: long,
+                        address: {
+                            city: city,
+                            region: region,
+                            country: country
+                        }
+                    }
+
+                    x++;
+
+                    if(x === events.length){
+                        response.setHeader('Content-Type', 'application/json');
+                        response.setHeader('Access-Control-Allow-Origin', '*');
+                        response.send(JSON.stringify({ events }));
+                        response.end();
+            
+                        console.log('events sent');
+                    }
+                }else{
+                    x++;
+                }  
+            });   
         }
-
-        response.setHeader('Content-Type', 'application/json');
-        response.setHeader('Access-Control-Allow-Origin', '*');
-        response.send(JSON.stringify({ events }));
-        response.end();
 
         if(err){
             console.error(err.message);
